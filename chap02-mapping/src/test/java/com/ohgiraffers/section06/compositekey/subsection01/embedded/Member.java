927cbbb04f9f5e656bef4799ab5e51e2b479e0f3
blimu <sheep.ohgiraffers@gmail.com>
2023-10-24 11:51:13 +0900

    commit

--- chap02-mapping/src/test/java/com/ohgiraffers/section06/compositekey/subsection01/embedded/Member.java
+++ chap02-mapping/src/test/java/com/ohgiraffers/section06/compositekey/subsection01/embedded/Member.java
@@ -1,2 +1,13 @@
-package com.ohgiraffers.section06.compositekey.subsection01.embedded;public class Member {
+package com.ohgiraffers.section06.compositekey.subsection01.embedded;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity(name="member_section06_subsection01")
+@Table(name="tbl_member_section06_subsection01")
+public class Member {
+
+    @EmbeddedId
+    private MemberPK memberPK;
 }
