f18a46cdd99845cd35a5b74e6a213c64eff68ace
blimu <sheep.ohgiraffers@gmail.com>
2023-10-24 12:30:38 +0900

    commit

--- chap02-mapping/src/test/java/com/ohgiraffers/section06/compositekey/subsection01/embedded/Member.java
+++ chap02-mapping/src/test/java/com/ohgiraffers/section06/compositekey/subsection01/embedded/Member.java
@@ -1,5 +1,6 @@
 package com.ohgiraffers.section06.compositekey.subsection01.embedded;
 
+import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
@@ -10,4 +11,52 @@ public class Member {
 
     @EmbeddedId
     private MemberPK memberPK;
+
+    @Column(name="phone")
+    private String phone;
+
+    @Column(name="address")
+    private String address;
+
+    public Member() {
+    }
+
+    public Member(MemberPK memberPK, String phone, String address) {
+        this.memberPK = memberPK;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public MemberPK getMemberPK() {
+        return memberPK;
+    }
+
+    public void setMemberPK(MemberPK memberPK) {
+        this.memberPK = memberPK;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "memberPK=" + memberPK +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
